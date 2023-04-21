d097c7fde465d4ad3acfed09cb6ab3eabd2acaa7
Mayrton Eduardo <mayrtontrabalho@gmail.com>
2023-04-20 22:39:58 -0300

    codigo dos quatro operadores matematiocos

---
+++ calculadora_python.py
@@ -0,0 +1,18 @@
+print("Calculadora Python")
+
+num1 = float(input("Digite o primeiro número: "))
+operador = input("Digite o operador '+, -, *, /': ")
+num2 = float(input("Digite o segundo número: "))
+
+if operador == '+':
+    resultado = num1 + num2
+elif operador == '-':
+    resultado = num1 - num2
+elif operador == '*':
+    resultado = num1 * num2
+elif operador == '/':
+    resultado = num1 / num2
+else:
+    print("Operador invalido")
+
+print("O resultado é", resultado)
