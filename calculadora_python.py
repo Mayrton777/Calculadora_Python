b9aa3ef15feb966b952d99243548ed8a9337c05c
Mayrton Eduardo <mayrtontrabalho@gmail.com>
2023-04-22 16:15:22 -0300

    atualizando

--- calculadora_python.py
+++ calculadora_python.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 import operacoes
 import sys
 
+# Dicionário com as operações disponíveis
 OPERACOES_DISPONIVEIS = {
     '+': operacoes.soma,
     '-': operacoes.sub,
@@ -12,8 +13,17 @@ OPERACOES_DISPONIVEIS = {
     'raiz': operacoes.raiz,
 }
 
-#Entrada
+# Função que obtem os valores digitados nos campos e realiza validações
 def obter_valores():
+    """
+    Obtém os valores digitados nos campos e realiza validações.
+
+    Raises:
+        ValueError: se algum valor digitado não for um número válido ou se o operador não for válido.
+
+    Returns:
+        tuple: um tuple contendo os valores numéricos e o operador.
+    """
     num1_text = num1_entry.get()
     num2_text = num2_entry.get()
     operador = operador_entry.get()
@@ -23,13 +33,27 @@ def obter_valores():
     except ValueError:
         raise ValueError("Valores inválidos! Por favor, digite números válidos.")
     if operador not in OPERACOES_DISPONIVEIS:
-        raise ValueError("Operador inválido! Por favor, digite um dos seguintes operadores: +, -, * ou **")
+        raise ValueError("Operador inválido! Por favor, digite um dos seguintes operadores: +, -, *, / ou **")
     if operador == '/' and num2 == 0:
         raise ValueError("Divisão por zero!")
     return num1, num2, operador
 
-#calculo da operação
+# Função que realiza a operação escolhida
 def calcular_operacao(num1, num2, operador):
+    """
+    Realiza a operação escolhida.
+
+    Args:
+        num1 (float): o primeiro número da operação.
+        num2 (float): o segundo número da operação.
+        operador (str): o operador da operação.
+
+    Raises:
+        ValueError: se o operador não for válido.
+
+    Returns:
+        float: o resultado da operação.
+    """
     if operador == 'raiz':
         return OPERACOES_DISPONIVEIS[operador](num1)
     return OPERACOES_DISPONIVEIS[operador](num1, num2)
@@ -39,10 +63,11 @@ def calcular():
     try:
         num1, num2, operador = obter_valores()
         resultado = calcular_operacao(num1, num2, operador)
-        resultado_label.configure(text="O resultado é: {:.2f}".format(resultado))
+        resultado_label.configure(text=f"O resultado é: {resultado:.2f}")
     except ValueError as e:
         tk.messagebox.showerror("Erro", str(e))
     except ZeroDivisionError as e:
+        # Verifica qual é o erro específico que ocorreu
         e = sys.exc_info()[1]
         if "Valor invalido" in str(e):
             tk.messagebox.showerror("Erro","Valor inválido! Por favor digite um número válido.")
@@ -53,15 +78,16 @@ def calcular():
         else:
             tk.messagebox.showerror("Erro","Erro desconhecido")
 
-#Função para calcular a raiz
+# Função que é executada quando o botão 'Raiz' é pressionado
 def calcular_raiz():
     try:
         num1 = float(num1_entry.get())
         resultado = operacoes.raiz(num1)
-        resultado_label.configure(text="A raiz quadrade de {} é {:.2f}".format(num1, resultado))
+        resultado_label.configure(text=f"A raiz quadrade de {num1} é {resultado:.2f}")
     except ValueError:
         tk.messagebox.showerror("Erro","Valor inválido! Por favor, digite um número válido.")
 
+# Cria uma nova janela
 janela = tk.Tk()
 janela.title("Calculadora Python")
 
@@ -76,7 +102,7 @@ calcular_button = tk.Button(janela, text="Calcular", command=calcular)
 raiz_button = tk.Button(janela, text="Raiz", command=calcular_raiz)
 resultado_label = tk.Label(janela, text="Resultado:")
 
-#Posição widgets
+#Posição dos widgets na janela
 num1_label.grid(row=0, column=0)
 num1_entry.grid(row=0, column=1)
 operador_label.grid(row=1, column=0)
