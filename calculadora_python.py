00eccfe2925eb782c43d8da06b4ee88f944b16e2
Mayrton Eduardo <mayrtontrabalho@gmail.com>
2023-04-21 23:34:34 -0300

    atualizando o codigo

--- calculadora_python.py
+++ calculadora_python.py
@@ -1,5 +1,28 @@
 import math
 
+#função operadores
+def soma(num1, num2):
+    return num1 + num2
+def sub(num1, num2):
+    return num1 - num2
+def mult(num1, num2):
+    return num1 * num2
+def div(num1, num2):
+    return num1 / num2
+def expo(num1, num2):
+    return num1 ** num2
+def raiz(num1):
+    return math.sqrt(num1)
+
+operacoes = {
+    '+': soma,
+    '-': sub,
+    '*': mult,
+    '/': div,
+    '**': expo,
+    'raiz': raiz,
+}
+
 print("Calculadora Python")
 
 #loop
@@ -16,30 +39,21 @@ while True:
         #Entrada operador
         operador = input("Digite o operador '+, -, *, /, **, raiz': ")
 
+        if operador not in operacoes:
+            print("Operador inválido!")
+            continue
+
         if operador == 'raiz':
-            resultado = math.sqrt(num1)
+            resultado = operacoes[operador](num1)
             print("A raiz de {} é {:.2f}".format(num1,resultado))
             continue
 
+        #Entrada num2
         num2 = float(input("Digite o segundo número: "))
 
-        #Operadores
-        if operador == '+':
-            resultado = num1 + num2
-        elif operador == '-':
-            resultado = num1 - num2
-        elif operador == '*':
-            resultado = num1 * num2
-        elif operador == '/':
-            resultado = num1 / num2
-        elif operador == '**':
-            resultado = num1 / num2
-        else:
-            print("Operador invalido")
-            resultado = None
-
         #Resultado
-        if resultado is not None:
-            print("O resultado é: {:.2f}".format(resultado))
+        resultado = operacoes[operador](num1,num2)
+        print("O resultado é: {:.2f}".format(resultado))
+
     except ValueError:
         print("Valor inválido! Por favor, digite um valor válido.")
