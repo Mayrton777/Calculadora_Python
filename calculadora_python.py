beff2f1847bd027d3613085e6f7b5aaf8577e292
Mayrton Eduardo <mayrtontrabalho@gmail.com>
2023-04-22 10:30:34 -0300

    adicionando erros

--- calculadora_python.py
+++ calculadora_python.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+from tkinter import messagebox
 import operacoes
+import sys
 
 OPERACOES_DISPONIVEIS = {
     '+': operacoes.soma,
@@ -10,31 +12,55 @@ OPERACOES_DISPONIVEIS = {
     'raiz': operacoes.raiz,
 }
 
+#Entrada
+def obter_valores():
+    num1_text = num1_entry.get()
+    num2_text = num2_entry.get()
+    operador = operador_entry.get()
+    try:
+        num1 = float(num1_text)
+        num2 = float(num2_text)
+    except ValueError:
+        raise ValueError("Valores inválidos! Por favor, digite números válidos.")
+    if operador not in OPERACOES_DISPONIVEIS:
+        raise ValueError("Operador inválido! Por favor, digite um dos seguintes operadores: +, -, * ou **")
+    if operador == '/' and num2 == 0:
+        raise ValueError("Divisão por zero!")
+    return num1, num2, operador
+
+#calculo da operação
+def calcular_operacao(num1, num2, operador):
+    if operador == 'raiz':
+        return OPERACOES_DISPONIVEIS[operador](num1)
+    return OPERACOES_DISPONIVEIS[operador](num1, num2)
+
 #Função do cálculo
 def calcular():
     try:
-        num1 = float(num1_entry.get())
-        num2 = float(num2_entry.get())
-        operador = operador_entry.get()
-
-        if operador not in OPERACOES_DISPONIVEIS:
-            resultado_label.configure(text="Operador inválido!")
-            return
-              
-        resultado = OPERACOES_DISPONIVEIS[operador](num1,num2)
+        num1, num2, operador = obter_valores()
+        resultado = calcular_operacao(num1, num2, operador)
         resultado_label.configure(text="O resultado é: {:.2f}".format(resultado))
-
-    except ValueError:
-        resultado_label.configure(text="Valor inválido! Por favor, digite um número válido.")
+    except ValueError as e:
+        tk.messagebox.showerror("Erro", str(e))
+    except ZeroDivisionError as e:
+        e = sys.exc_info()[1]
+        if "Valor invalido" in str(e):
+            tk.messagebox.showerror("Erro","Valor inválido! Por favor digite um número válido.")
+        elif "Divisão por zero" in str(e):
+            tk.messagebox.showerror("Erro","Divisão por zero!")
+        elif "Operador invalido" in str(e):
+            tk.messagebox.showerror("Erro","Operador inválido!")
+        else:
+            tk.messagebox.showerror("Erro","Erro desconhecido")
 
 #Função para calcular a raiz
 def calcular_raiz():
     try:
         num1 = float(num1_entry.get())
-        resultado = OPERACOES_DISPONIVEIS['raiz'](num1)
+        resultado = operacoes.raiz(num1)
         resultado_label.configure(text="A raiz quadrade de {} é {:.2f}".format(num1, resultado))
     except ValueError:
-        resultado_label.config(text="Valor inválido! Por favor, digite um número válido.")
+        tk.messagebox.showerror("Erro","Valor inválido! Por favor, digite um número válido.")
 
 janela = tk.Tk()
 janela.title("Calculadora Python")
@@ -61,5 +87,5 @@ calcular_button.grid(row=3, column=0, columnspan=2)
 raiz_button.grid(row=3, column=1, columnspan=2)
 resultado_label.grid(row=4, column=0, columnspan=2)
 
- #inicia o loop principal da janela
+#inicia o loop principal da janela
 janela.mainloop()
