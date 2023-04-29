cc477381ce6a7934b4be67abba19c5b8cd33994d
Mayrton Eduardo <mayrtontrabalho@gmail.com>
2023-04-29 00:10:04 -0300

    novo botão

--- calculadora_python.py
+++ calculadora_python.py
@@ -7,9 +7,9 @@ import sys
 OPERACOES_DISPONIVEIS = {
     '+': operacoes.soma,
     '-': operacoes.sub,
-    '*': operacoes.mult,
+    'x': operacoes.mult,
     '/': operacoes.div,
-    '**': operacoes.expo,
+    '^': operacoes.expo,
     'raiz': operacoes.raiz,
 }
 
@@ -58,7 +58,7 @@ def calcular_operacao(num1, num2, operador):
         return OPERACOES_DISPONIVEIS[operador](num1)
     return OPERACOES_DISPONIVEIS[operador](num1, num2)
 
-#Função do cálculo
+# Função do cálculo
 def calcular():
     try:
         num1, num2, operador = obter_valores()
@@ -87,31 +87,56 @@ def calcular_raiz():
     except ValueError:
         tk.messagebox.showerror("Erro","Valor inválido! Por favor, digite um número válido.")
 
+# Função para limpar
+def clear():
+    num1_entry.delete(0, tk.END)
+    num2_entry.delete(0, tk.END)
+    operador_entry.delete(0, tk.END)
+    
 # Cria uma nova janela
 janela = tk.Tk()
 janela.title("Calculadora Python")
 
-#Widgets
+# Tamanho da janela
+janela.geometry("363x100")
+
+# Botões da calculadora
+soma_button = tk.Button(janela, text="+", width=5, command=lambda: operador_entry.insert(tk.END, '+'))
+sub_button = tk.Button(janela, text="-", width=5, command=lambda: operador_entry.insert(tk.END, '-'))
+mult_button = tk.Button(janela, text="x", width=5, command=lambda: operador_entry.insert(tk.END, 'x'))
+div_button = tk.Button(janela, text="/", width=5, command=lambda: operador_entry.insert(tk.END, '/'))
+expo_button = tk.Button(janela, text="^", width=5, command=lambda: operador_entry.insert(tk.END, '^'))
+raiz_button = tk.Button(janela, text="Raiz", width=5, command=calcular_raiz)
+calcular_button = tk.Button(janela, text="=",width=5, command=calcular)
+clear_button = tk.Button(janela, text="C",width=5, command=clear)
+
+# Posição botões da calculadora
+soma_button.grid(row=0, column=2)
+sub_button.grid(row=0, column=3)
+mult_button.grid(row=1, column=2)
+div_button.grid(row=1, column=3)
+expo_button.grid(row=1, column=4)
+raiz_button.grid(row=2, column=3)
+calcular_button.grid(row=2, column=2)
+clear_button.grid(row=0, column=4)
+
+# Widgets
 num1_label = tk.Label(janela, text="Primeiro número:")
 num1_entry = tk.Entry(janela)
 operador_label = tk.Label(janela, text="Operador:")
 operador_entry = tk.Entry(janela)
 num2_label = tk.Label(janela, text="Segundo número:")
 num2_entry = tk.Entry(janela)
-calcular_button = tk.Button(janela, text="Calcular", command=calcular)
-raiz_button = tk.Button(janela, text="Raiz", command=calcular_raiz)
 resultado_label = tk.Label(janela, text="Resultado:")
 
-#Posição dos widgets na janela
+# Posição dos widgets na janela
 num1_label.grid(row=0, column=0)
 num1_entry.grid(row=0, column=1)
 operador_label.grid(row=1, column=0)
 operador_entry.grid(row=1, column=1)
 num2_label.grid(row=2, column=0)
 num2_entry.grid(row=2, column=1)
-calcular_button.grid(row=3, column=0, columnspan=2)
-raiz_button.grid(row=3, column=1, columnspan=2)
 resultado_label.grid(row=4, column=0, columnspan=2)
 
-#inicia o loop principal da janela
+# inicia o loop principal da janela
 janela.mainloop()
